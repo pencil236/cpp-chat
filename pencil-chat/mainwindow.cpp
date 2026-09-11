388d65e8ee561527b822116c37b077ec74fa6873
pencil <206560977+pencil236@users.noreply.github.com>
2026-09-12 04:13:33 +0800

    feat: add pencil-chat static login interface

---
+++ pencil-chat/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "logindialog.h"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    _login_dlg = new LoginDialog();
+    setCentralWidget(_login_dlg);
+    _login_dlg->show();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
